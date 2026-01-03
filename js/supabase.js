c03efa5b4f82afc37b5a9535b73d657cf3cb3246
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2026-01-03 10:50:28 +0530

    Add Supabase client initialization script

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+<script>
+const supabaseUrl = "PASTE_YOUR_SUPABASE_URL";
+const supabaseKey = "PASTE_YOUR_ANON_PUBLIC_KEY";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+</script>

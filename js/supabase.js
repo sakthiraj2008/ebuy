3e8699181d5123b24cf045016aecbfa17a91a4af
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2026-01-03 11:34:47 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
 <script>
-const supabaseUrl = "PASTE_YOUR_SUPABASE_URL";
-const supabaseKey = "PASTE_YOUR_ANON_PUBLIC_KEY";
+const supabaseUrl = "https://uvmuiiyjhldqzduusrja.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2bXVpaXlqaGxkcXpkdXVzcmphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNDk0NzgsImV4cCI6MjA4MjkyNTQ3OH0.IOgAcW8ysWrWegf4OfTX6p73IlpJ5ZuEARbAla282rA";
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 </script>
